6f2091772ea405a48f85d84f17c46630fd618892
Mateus Melo <msmelo95@gmail.com>
2021-02-05 21:33:03 -0300

    Adicionando arquivos e criando o CRUD dos usuarios do sistema

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-param-reassign': 0,
+    'class-methods-use-this': 0,
+    camelcase: 0,
+  },
+};
---
+++ src/controllers/UserController.js
@@ -0,0 +1,91 @@
+import User from '../models/UsersModel';
+
+class UserController {
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+
+      return res.json(users);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const newUser = await User.create(req.body);
+      const { id, name, email } = newUser;
+      return res.json({
+        id,
+        name,
+        email,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing id'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User not found'],
+        });
+      }
+
+      const newUser = await user.update(req.body);
+
+      return res.json(newUser);
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Missing id'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User not found'],
+        });
+      }
+
+      const { name, email } = await user.destroy();
+
+      return res.json({
+        name,
+        email,
+        deleted: true,
+      });
+    } catch (err) {
+      return res.status(400).json({
+        errors: err.errors.map((error) => error.message),
+      });
+    }
+  }
+}
+
+export default new UserController();
